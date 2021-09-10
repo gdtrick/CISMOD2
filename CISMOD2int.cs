09bf819f78f5d06a7bf9170a2bc41d5efee43471
gdtrick <89590267+gdtrick@users.noreply.github.com>
2021-09-10 10:06:43 -0400

    Add files via upload

---
+++ CISMOD2int.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CISMOD2INT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Begins execution
+
+            int[] playersnumbers = { 7, 54, 15, 12, 10, 1, 16, 33, 22, 58 };
+            Console.WriteLine(playersnumbers[1]);
+            Console.WriteLine(playersnumbers[5]);
+            Console.WriteLine(playersnumbers[7]);
+
+            //comment out code
+        }
+    }
+}
