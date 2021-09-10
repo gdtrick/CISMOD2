c9fa228243b705f910989c3b6d59dc7e97fb0e18
gdtrick <89590267+gdtrick@users.noreply.github.com>
2021-09-10 10:10:31 -0400

    Add files via upload

---
+++ CISMOD2dbl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CISMOD2dbl
+{
+    class Program
+    {
+        static void Program.Main(double[] args)
+        {
+
+            double[] nflteams = { "Steelers", "Colts", "Chiefs", "Bills", "Titans", "Falcons", "Jaguars", "Vikings", "Panthers", "Broncos" };
+            Console.WriteLine(nflteams[1]);
+            Console.WriteLine(nflteams[5]);
+            Console.WriteLine(nflteams[7]);
+
+            for (int i=0; i < nflteams.Length;i++)
+            {
+                Console.WriteLine(nflteams[i]);
+            }
+         
+        }
+    }
+}
